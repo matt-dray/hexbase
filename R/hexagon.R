4cb88e7501bfdd0aa05ec344df9b213063a7d0c8
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2025-01-31 21:48:43 +0000

    Simplify function set, rely on grid

--- R/hexagon.R
+++ R/hexagon.R
@@ -1,51 +1,59 @@
-#' Write a Hex Sticker to File
+#' Open a Device
 #'
-#' Generate a simple hexagon-shaped sticker design with a border, image and
-#' text.
+#' Begin a PNG plot device with dimensions matching
+#' [the Stickers Standard](https://sticker.how/#type-hexagon): 4.39 cm wide by
+#' 5.08 cm high (2 by 1.73 inches).
 #'
 #' @param file_path Character. Full file path to a .png where the output file
 #'     will be saved. The containing directory must already exist.
-#' @param file_open Logical. Open the PNG file once it's written?
-#' @param border_width Numeric. Thickness of the border, expressed as a ratio
-#'     of the 'inner' hexagon to 'outer' hexagon (must be less than 1).
+#'
+#' @returns Nothing. A graphics device is opened.
+#'
+#' @export
+#'
+#' @examples
+#' temp_path <- tempfile(fileext = ".png")
+#' open_device(temp_path)
+#' add_hex()
+#' image_path <- system.file("img", "Rlogo.png", package = "png")
+#' image_png <- png::readPNG(image_path)
+#' add_image(image_png)
+#' add_text()
+#' close_device()
+open_device <- function(file_path) {
+
+  if (grepl(file_path, "\\.png$")) {
+    stop("Argument 'file_path' must end with '.png'.")
+  }
+
+  if (!dir.exists(dirname(file_path))) {
+    stop("Argument 'file_path' must resolve to an existing directory.")
+  }
+
+  grDevices::png(
+    filename = file_path,
+    width =  4.39,
+    height = 5.08,
+    units = "cm",
+    res = 1200,
+    bg = "transparent"
+  )
+
+}
+
+#' Add a Hexagon with a Border
+#'
+#' Add two hexagons, where the exposed region between the two creates a border.
+#'
+#' @param border_width Numeric. Thickness of the border, expressed as the
+#'     inverse ratio of the 'inner' hexagon to 'outer' hexagon (must be between
+#'     `0` and `1`).
 #' @param border_col Character. Named R colour or hexadecimal code for the
 #'     border around the hex.
 #' @param bg_col Character. Named R colour or hexadecimal code for the interior
 #'     background.
-#' @param txt_string Character. Text to display. `NULL` (or an empty string) if
-#'    you don't want to place text.
-#' @param txt_x Numeric. Text location x-axis.
-#' @param txt_y Numeric. Text location y-axis.
-#' @param txt_angle Numeric. Rotation of text string in degrees. Positive values
-#'     will rotate anticlockwise by the given angle.
-#' @param txt_size Numeric. Text point-size.
-#' @param txt_col Character. Text colour. A named R colour or hexadecimal code.
-#' @param txt_font Character. Name of a font family available on your system.
-#' @param img_object Array. A PNG file read in by the user. `NULL` for no image.
-#' @param img_x Numeric. Image location x-axis.
-#' @param img_y Numeric. Image location y-axis.
-#' @param img_width Numeric. Image width.
-#' @param img_height Numeric. Image height.
-#' @param img_angle Numeric. Text rotation in degrees.
 #'
 #' @details
-#'
-#' ## Dimensions
-#'
-#' Writes a hexagon to the dimensions of
-#' [the Stickers Standard](https://sticker.how/#type-hexagon): 4.39 cm wide by
-#' 5.08 cm high (2 by 1.73 inches).
-#'
-#' Any elements falling outside of the hexagon will be clipped out.
-#'
-#' ## Coordinates
-#'
-#' Coordinates should be provided as native units ('Normalised Parent
-#' Coordinates'), which means that the x- and y-axes range from 0 to 1 with the
-#' centre at x = 0.5 and y = 0.5.
-#'
-#' This applies to arguments `txt_x`, `txt_y`, `img_x`, `img_y`, `img_width`
-#' and `img_height`.
 #'
 #' ## Colours
 #'
@@ -53,105 +61,283 @@
 #' colour values must be provided with length 6 or 8 and must begin with an
 #' octothorpe (`#`).
 #'
-#' This applies to arguments `border_col`, `bg_col` and `txt_col`.
-#'
-#' ## Write order
-#'
-#' Each subsequent element overlays the last. The order of action when building
-#' the hex is:
-#'
-#' 1. Add outer hexagon.
-#' 2. Add inner hexagon.
-#' 3. Add image.
-#' 4. Add text.
-#' 5. Clip to outer hexagon.
-#'
-#' @return Nothing.
+#' @returns `NULL`. Adds to an existing graphics device.
 #'
 #' @export
 #'
 #' @examples
-#' tmp <- tempfile(fileext = ".png")
-#' make_hex(file_path = tmp)
-make_hex <- function(
-    file_path,
-    file_open = FALSE,
-    border_width = 0.95,
-    border_col = "blue",
-    bg_col = "grey80",
-    txt_string = "example\ntext",
-    txt_x = 0.5,
-    txt_y = 0.5,
-    txt_angle = 30,
-    txt_size = 20,
-    txt_col = "red",
-    txt_font = "mono",
-    img_object = NULL,
-    img_x = 0.5,
-    img_y = 0.5,
-    img_width = 0.5,
-    img_height = 0.5,
-    img_angle = 0
+#' temp_path <- tempfile(fileext = ".png")
+#' open_device(temp_path)
+#' add_hex()
+#' image_path <- system.file("img", "Rlogo.png", package = "png")
+#' image_png <- png::readPNG(image_path)
+#' add_image(image_png)
+#' add_text()
+#' close_device()
+add_hex <- function(
+    border_width = 0.05,
+    border_col = "black",
+    bg_col = "grey"
 ) {
 
-  grDevices::png(
-    filename = file_path,
-    width = 4.39,
-    height = 5.08,
-    units = "cm",
-    res = 1200,
-    bg = "transparent"
+  if (!inherits(border_width, "numeric") || border_width >= 1) {
+    stop("Argument 'border_width' must be a numeric value below 1.")
+  }
+
+  if (!(border_col %in% grDevices::colours() | grepl("^#[0-9A-Fa-f]{6}$", border_col))) {
+    stop("Argument 'border_col' must a named R colour or a hex code.")
+  }
+
+  if (!(bg_col %in% grDevices::colours() | grepl("^#[0-9A-Fa-f]{6}$", bg_col))) {
+    stop("Argument 'bg_col' must a named R colour or a hex code.")
+  }
+
+  hex_diameter_inner <- 1 - border_width
+
+  hex_coords_outer <- .get_hex_coords(diameter = 1)
+  hex_coords_inner <- .get_hex_coords(diameter = hex_diameter_inner)
+
+  x_scale <- c(min(hex_coords_outer[["x"]]), max(hex_coords_outer[["x"]]))
+  y_scale <- c(min(hex_coords_outer[["y"]]), max(hex_coords_outer[["y"]]))
+
+  grid::pushViewport(grid::viewport(xscale = x_scale, yscale = y_scale))
+
+  hex_grob_outer <- grid::polygonGrob(
+    hex_coords_outer[["x"]],
+    hex_coords_outer[["y"]],
+    gp = grid::gpar(lwd = 0, fill = border_col),
+    default.units = "native"
+  )
+  grid::pushViewport(
+    grid::viewport(
+      xscale = x_scale,
+      yscale = y_scale,
+      clip = hex_grob_outer
+    )
   )
 
-  coords_outer <- .get_hex_coords(d = 1)
-  coords_outer_scaled <- lapply(coords_outer, .scale_outer_coords)
-  grob_outer <- .engrob_hex(coords_outer_scaled, border_col)
+  grid::grid.draw(hex_grob_outer)
 
-  coords_inner <- .get_hex_coords(d = border_width)
-  coords_inner_scaled <- .get_inner_hex_coords_scaled(coords_inner, coords_outer)
-  grob_inner <- .engrob_hex(coords_inner_scaled, bg_col)
+  grid::grid.polygon(
+    x = hex_coords_inner[["x"]],
+    y = hex_coords_inner[["y"]],
+    default.units = "native",
+    gp = grid::gpar(lwd = 0, fill = bg_col)
+  )
 
-  if (!is.null(img_object)) {
-    grob_img <- grid::rasterGrob(img_object, width = img_width, height = img_height)
+}
+
+#' Get Coordinates of Hexagon Vertices
+#' @param diameter Numeric.
+#' @returns A list of two numeric vectors that represent points of a hexagon.
+#'     The elements are named 'x' and 'y'.
+#' @noRd
+.get_hex_coords <- function(diameter = 1) {
+
+  radius <- diameter / 2
+  centre <- 0.5
+  angles <- seq(0, 2 * pi, length.out = 7)
+
+  list(
+    x = centre + radius * cos(angles - pi / 6),
+    y = centre + radius * sin(angles - pi / 6)
+  )
+
+}
+
+#' Add Text
+#'
+#' Overlay text on the hexagon. Text outside the hexagon will be clipped. Should
+#' be called after [open_device] and [add_hex], in that order. Call this function
+#' separately for each text item that you want to add.
+#'
+#' @param text_string Character. Text to display. `NULL` (or an empty string) if
+#'    you don't want to place text.
+#' @param text_x Numeric. Text location x-axis.
+#' @param text_y Numeric. Text location y-axis.
+#' @param text_angle Numeric. Rotation of text string in degrees. Positive
+#'     values will rotate anticlockwise by the given angle.
+#' @param text_size Numeric. Text point-size.
+#' @param text_col Character. Text colour. A named R colour or hexadecimal code.
+#' @param text_family Character. Name of a font family available on your system.
+#' @param text_face Character. Font face for the text.
+#'
+#' @details
+#'
+#' ## Coordinates
+#'
+#' Coordinates should be provided within the x- and y-axis ranges, which are
+#' both from 0 to 1, giving the centre as x = 0.5 and y = 0.5.
+#'
+#' ## Colours
+#'
+#' Named colour values must be listed in [grDevices::colours()]. Hexadecimal
+#' colour values must be provided with length 6 or 8 and must begin with an
+#' octothorpe (`#`).
+#'
+#' @returns `NULL`. Adds to an existing graphics device.
+#'
+#' @export
+#'
+#' @examples
+#' temp_path <- tempfile(fileext = ".png")
+#' open_device(temp_path)
+#' add_hex()
+#' image_path <- system.file("img", "Rlogo.png", package = "png")
+#' image_png <- png::readPNG(image_path)
+#' add_image(image_png)
+#' add_text()
+#' close_device()
+add_text <- function(
+    text_string = "example",
+    text_x = 0.5,
+    text_y = 0.3,
+    text_angle = 0,
+    text_size = 20,
+    text_col = "black",
+    text_family = "sans",
+    text_face = c("plain", "bold", "italic", "bold.italic")
+) {
+
+  if (!inherits(text_string, "character")) {
+    stop("Argument 'text_string' must be a character string.")
   }
 
-  if (!is.null(txt_string)) {
-    grob_txt <- .engrob_text(txt_string, txt_col, txt_size, txt_font)
+  if (!inherits(text_x, "numeric") | !inherits(text_y, "numeric")) {
+    stop("Arguments 'text_x' and 'text_y' must be numeric values.")
   }
 
-  vp_clip <- grid::viewport(clip = grob_outer)
-  grid::pushViewport(vp_clip)
-
-  for (grob in list(grob_outer, grob_inner)) {
-    vp_hex <- grid::viewport(gp = grid::gpar(lwd = 0))
-    grid::pushViewport(vp_hex)
-    grid::grid.draw(grob)
+  if (!inherits(text_size, "numeric")) {
+    stop("Argument 'text_size' must be a numeric value.")
   }
 
-  if (!is.null(img_object)) {
-    vp_img_parent <- grid::viewport(x = img_x, y = img_y)
-    grid::pushViewport(vp_img_parent)
-    vp_img <- grid::viewport(angle = img_angle)
-    grid::pushViewport(vp_img)
-    grid::grid.draw(grob_img)
-    grid::popViewport()
-    grid::popViewport()
+  if (!inherits(text_size, "numeric")) {
+    stop("Argument 'text_size' must be a numeric value.")
   }
 
-  if (!is.null(txt_string)) {
-    vp_txt_parent <- grid::viewport(x = txt_x, y = txt_y)
-    grid::pushViewport(vp_txt_parent)
-    vp_txt <- grid::viewport(angle = txt_angle)
-    grid::pushViewport(vp_txt)
-    grid::grid.draw(grob_txt)
-    grid::popViewport()
-    grid::popViewport()
+  if (!(text_col %in% grDevices::colours() | grepl("^#[0-9A-Fa-f]{6}$", text_col))) {
+    stop("Argument 'text_col' must a named R colour or a hex code.")
   }
 
-  grid::popViewport()  # clip
+  if (!inherits(text_family, "character")) {
+    stop(
+      "Argument 'text_family' must be a character string
+      representing an available font family."
+    )
+  }
 
+  if (!inherits(text_face, "character")) {
+    stop(
+      "Argument 'text_face' must be one of the following character strings:
+      'plain', 'bold', 'italic', 'bold-italic'."
+    )
+  }
+
+  text_face <- match.arg(text_face)
+
+  grid::pushViewport(grid::viewport(x = text_x, y = text_y))
+  grid::grid.text(
+    text_string,
+    gp = grid::gpar(
+      col = text_col,
+      fontsize = text_size,
+      fontfamily = text_family,
+      fontface = text_face
+    ),
+    vp = grid::viewport(angle = text_angle)
+  )
+  grid::popViewport()
+
+
+}
+
+#' Add an Image
+#'
+#' Overlay an image on the hexagon. Image content outside the hexagon will be
+#' clipped. Should be called after [open_device] and [add_hex], in that order.
+#' Call this function separately for each image you want to add.
+#'
+#' @param image_object Array. A PNG file read in by the user. `NULL` for no
+#'     image.
+#' @param image_x Numeric. Image location x-axis.
+#' @param image_y Numeric. Image location y-axis.
+#' @param image_angle Numeric. Text rotation in degrees.
+#' @param image_width Numeric. Image width.
+#'
+#' @details
+#'
+#' ## Coordinates
+#'
+#' Coordinates should be provided within the x- and y-axis ranges, which are
+#' both from 0 to 1, giving the centre as x = 0.5 and y = 0.5.
+#'
+#' @returns `NULL`. Adds to an existing graphics device.
+#'
+#' @export
+#'
+#' @examples
+#' temp_path <- tempfile(fileext = ".png")
+#' open_device(temp_path)
+#' add_hex()
+#' image_path <- system.file("img", "Rlogo.png", package = "png")
+#' image_png <- png::readPNG(image_path)
+#' add_image(image_png)
+#' add_text()
+#' close_device()
+add_image <- function(
+    image_object,
+    image_x = 0.5,
+    image_y = 0.7,
+    image_angle = 0,
+    image_width = 0.4
+) {
+
+  if (!inherits(image_x, "numeric") | !inherits(image_y, "numeric")) {
+    stop("Arguments 'image_x' and 'image_y' must be numeric values.")
+  }
+
+  if (!inherits(image_angle, "numeric")) {
+    stop("Argument 'image_angle' must be a numeric value.")
+  }
+
+  if (!inherits(image_width, "numeric")) {
+    stop("Argument 'image_width' must be a numeric value.")
+  }
+
+  grid::pushViewport(
+    grid::viewport(
+      x = image_x,
+      y = image_y,
+      width = image_width
+    )
+  )
+  grid::grid.raster(
+    image_object,
+    vp = grid::viewport(angle = image_angle)
+  )
+  grid::popViewport()
+
+}
+
+#' Close the Device
+#'
+#' Clip to the area of the outer hexagon and close the PNG plot device, which
+#' writes to the `file_path` specified in [open_device].
+#'
+#' @returns `NULL`. Adds to an existing graphics device.
+#'
+#' @export
+#'
+#' @examples
+#' temp_path <- tempfile(fileext = ".png")
+#' open_device(temp_path)
+#' add_hex()
+#' image_path <- system.file("img", "Rlogo.png", package = "png")
+#' image_png <- png::readPNG(image_path)
+#' add_image(image_png)
+#' add_text()
+#' close_device()
+close_device <- function() {
+  grid::popViewport(0)  # clip to outer hexagon
   grDevices::dev.off()
-
-  if (file_open) system(paste("open", file_path))
-
 }
